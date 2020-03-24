0b31fe100cb657972e089c7cff72d34f6654b77d
isaacsquires <isaacsquires@me.com>
2020-03-24 15:53:46 +0000

    resizing logo

--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import './header.css'
-import hoverLogo from '../../functions/hoverLogo.js'
 
 
 
@@ -9,7 +8,7 @@ const Header = () => (
     <div id='header' className="header">
         <div className='row'>
             <div className='col-lg-offset-3 col-md-offset-3 col-lg-6 col-md-6 col-sm-5'>
-                <object id="logo" data={require('../../media/bbs-associates-logo-no-back.svg')} alt='BBS Associates'  type="image/svg+xml" onMouseEnter={() => { hoverLogo(); }}></object>
+                <object id="logo" data={require('../../media/bbs-associates-logo-no-back.svg')} alt='BBS Associates'  type="image/svg+xml"></object>
                 {/* <img id='logo' className='logo' alt='BBS Associates' src={require('../../media/bbs-associates-logo-no-back.svg')} onMouseEnter={() => { hoverLogo(); }}></img> */}
                 <div className="card text-center">
                 Accountants and Taxation Consultants
