08898e69f67203cec90973b74c9d9ccffe5a251b
isaacsquires <isaacsquires@me.com>
2020-03-24 15:25:00 +0000

    Updates

---
+++ precache-manifest.92eab014591c5aff0acdef377c6ddb0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1806fe37987486eb40de48143694e2b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d500c6d36e6c58e7d3cc",
+    "url": "./static/css/main.161e3126.chunk.css"
+  },
+  {
+    "revision": "246a4a5c20036c06cf4b",
+    "url": "./static/js/2.87c16370.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.87c16370.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d500c6d36e6c58e7d3cc",
+    "url": "./static/js/main.98123711.chunk.js"
+  },
+  {
+    "revision": "88e7c43f4fb5dd621910",
+    "url": "./static/js/runtime-main.61ac484a.js"
+  },
+  {
+    "revision": "7c40ad84dfc5c907965e14e4af9ee1c3",
+    "url": "./static/media/bbs-associates-logo-no-back.7c40ad84.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bbsassociates/precache-manifest.b52895ddfba2953bc05abc2d80fdddad.js"
+  "./precache-manifest.92eab014591c5aff0acdef377c6ddb0b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bbsassociates/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
